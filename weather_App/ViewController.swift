d74545e465254a3733fe81c2daa81d49b2ad5b10
osvaldo <osvaldo.1963@hotmail.com>
2016-11-02 23:25:02 -0700

    project added

---
+++ weather_App/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  weather_App
+//
+//  Created by osvaldo lopez on 11/2/16.
+//  Copyright © 2016 osvaldo lopez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
