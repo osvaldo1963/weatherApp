ee43f48a98a7a6152271e0bc9f29573749e132e2
osvaldo <osvaldo.1963@hotmail.com>
2016-11-04 11:53:20 -0700

    ui table and cell ready

--- weather_App/ViewController.swift
+++ weather_App/ViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController{
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
     @IBOutlet weak var dateLabel   : UILabel!
     @IBOutlet weak var temperatur  : UILabel!
     @IBOutlet weak var location    : UILabel!
@@ -19,7 +19,21 @@ class ViewController: UIViewController{
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        self.CustomTable.delegate = self
+        self.CustomTable.dataSource = self
+        self.CustomTable.rowHeight = 70
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 6
     }
 
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "watherCell", for: indexPath)
+        return cell
+    }
 }
 
